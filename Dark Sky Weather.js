593c69fa7eef33eccf3932dc8e7a4aa1e6a3306b
ikaikastine <ikaikastine@gmail.com>
2018-10-23 15:36:14 -0700

    Updated with dark sky api url

--- Dark Sky Weather.js
+++ Dark Sky Weather.js
@@ -1,3 +1,6 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: gray; icon-glyph: magic;
+let url = "https://api.darksky.net/forecast/abd7ff977eeea6f82b5b0948b1759beb/44.0505,123.0951"
+let req = new Request(url)
+let json = await req.loadJSON()
